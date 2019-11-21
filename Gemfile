ed45e647d3a4992d04068682adb35620892a281e
Ryan Palo <ryan@thepalos.com>
2019-11-21 15:08:59 -0800

    Tweak other pages and styles, rebuild, and redeploy

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-gem "jekyll", "~> 3.7.3"
+gem "jekyll"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- build/js/index.js
+++ build/js/index.js
@@ -5,10 +5,21 @@ tiles.some(function(tile) {
   const clickable = document.querySelector(`.tile__${tile}`);
   if (clickable) {
     clickable.addEventListener("click", function() {
-      window.location = "services/";
+      window.location = `/services/${tile}/`;
       return !BREAK;
     });
   } else {
     return BREAK;
+  }
+});
+
+// Dropdown Services Menu
+
+const servicesMenu = document.querySelector(".dropdown");
+servicesMenu.addEventListener("click", function() {
+  if (this.dataset["state"] == "open") {
+    this.dataset["state"] = "closed";
+  } else {
+    this.dataset["state"] = "open";
   }
 });
